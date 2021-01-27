dedeae56946cb62bc69ba8ae50c0ae75b6658d1b
OMEN <mrh61037@gmail.com>
2021-01-27 21:29:36 +0200

    update Utility.kt

--- app/src/main/java/com/nurbk/ps/projectm/utils/Utility.kt
+++ app/src/main/java/com/nurbk/ps/projectm/utils/Utility.kt
@@ -2,6 +2,7 @@ package com.nurbk.ps.projectm.utils
 
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
 import android.net.Uri
 import android.provider.MediaStore
 import android.widget.TextView
@@ -10,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.nurbk.ps.projectm.utils.FileHelper
 import java.io.File
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -40,4 +42,45 @@ object Utility {
     fun makeText(activity: AppCompatActivity, string: String) {
         Toast.makeText(activity, string, Toast.LENGTH_LONG).show()
     }
+
+
+
+    fun getMediaPath(context: Context, uri: Uri): String {
+
+        val resolver = context.contentResolver
+        val projection = arrayOf(MediaStore.Video.Media.DATA)
+        var cursor: Cursor? = null
+        try {
+            cursor = resolver.query(uri, projection, null, null, null)
+            return if (cursor != null) {
+                val columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATA)
+                cursor.moveToFirst()
+                cursor.getString(columnIndex)
+
+            } else ""
+
+        } catch (e: Exception) {
+            resolver.let {
+                val filePath = (context.applicationInfo.dataDir + File.separator
+                        + System.currentTimeMillis())
+                val file = File(filePath)
+
+                resolver.openInputStream(uri)?.use { inputStream ->
+                    FileOutputStream(file).use { outputStream ->
+                        val buf = ByteArray(4096)
+                        var len: Int
+                        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(
+                            buf,
+                            0,
+                            len
+                        )
+                    }
+                }
+                return file.absolutePath
+            }
+        } finally {
+            cursor?.close()
+        }
+    }
+
 }
