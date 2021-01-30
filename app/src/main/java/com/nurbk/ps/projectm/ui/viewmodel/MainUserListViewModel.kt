253aa26f606f5d7b5c91f8f1d6c135d0bf0d6b17
OMEN <mrh61037@gmail.com>
2021-01-30 17:59:58 +0200

    update MainViewModel

--- app/src/main/java/com/nurbk/ps/projectm/ui/viewmodel/MainUserListViewModel.kt
+++ app/src/main/java/com/nurbk/ps/projectm/ui/viewmodel/MainUserListViewModel.kt
@@ -3,6 +3,7 @@ package com.nurbk.ps.projectm.ui.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.nurbk.ps.projectm.model.User
 import com.nurbk.ps.projectm.repository.MainUserListRepository
 import com.nurbk.ps.projectm.repository.SignInRepository
 
@@ -12,9 +13,10 @@ class MainUserListViewModel(application: Application) : AndroidViewModel(applica
     private val mainUserRepository = MainUserListRepository(application.applicationContext)
 
     val getAllUserLiveData = mainUserRepository.getAllUserLiveData
+    val getAllGroupLiveData = mainUserRepository.getAllGroupLiveData
 
     fun updateUser(
-        map: Map<String, String>,
+        map: Map<String, Any>,
         id: String,
         collectionName: String,
         onComplete: () -> Unit
@@ -27,14 +29,20 @@ class MainUserListViewModel(application: Application) : AndroidViewModel(applica
 
     fun getLogOut() = mainUserRepository.logOut()
 
-     fun getProfile(onComplete: () -> Unit) =
+    fun getProfile(onComplete: () -> Unit) =
         mainUserRepository.signInRepository.getProfileData(
             FirebaseAuth.getInstance().currentUser!!.uid,
             onComplete
         )
 
+    fun addGroup(
+        currentUser: String,
+        groupChannel: User
+    ) = mainUserRepository.addGroup(currentUser, groupChannel)
+
 
     init {
         mainUserRepository.getAllUser()
+        mainUserRepository.getAllGroup()
     }
 }
