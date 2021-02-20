09f31fc65f5cd63d17284259869ea1e1791c9c06
OMEN <mrh61037@gmail.com>
2021-02-20 16:00:22 +0200

    update Sign in Auth View Model

--- app/src/main/java/com/nurbk/ps/projectm/ui/viewmodel/SignInAuthViewModel.kt
+++ app/src/main/java/com/nurbk/ps/projectm/ui/viewmodel/SignInAuthViewModel.kt
@@ -4,17 +4,24 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.nurbk.ps.projectm.model.User
 import com.nurbk.ps.projectm.repository.SignInRepository
+import com.nurbk.ps.projectm.repository.SignUpRepository
 
 class SignInAuthViewModel(application: Application) : AndroidViewModel(application) {
 
 
     private val sigInRepository = SignInRepository(application.applicationContext)
+    private val signUpRepository = SignUpRepository(application.applicationContext)
 
 
     fun signInWithEmailAndPassword(email: String, password: String) =
         sigInRepository.signIn(email = email, password = password)
 
+
     fun getSignIn() = sigInRepository.getSignIn()
 
-
+    fun insertUsers(users: User) {
+        signUpRepository.insertUser(users)
+        sigInRepository.getProfileData(users.id) {
+        }
+    }
 }
